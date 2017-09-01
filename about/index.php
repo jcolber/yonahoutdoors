c0922883c8ffc266a8d079f37c643aee3f71c191
Josh Colbert <jcolber@gmail.com>
2017-08-31 20:55:12 -0600

    Add files via upload

---
+++ about/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+
+    <title>We are Yonah and Yonah Loves You!</title>
+
+    <?php include("../templates/head.php"); ?>
+
+
+</head>
+
+<body>
+
+
+    <?php include("../templates/navbar.php"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Introduction Row -->
+        <h1 class="my-4">About Us <small>It's Nice to Meet You!</small></h1>
+        <p>The YONAH Outdoors Name: We want to set ourselves apart from the pack and that is why we chose the name YONAH Outdoors. Mount Yonah, a small mountain located in north Georgia, stands apart from the rest of the Blue Ridge mountain range. With a 1400 foot change in elevation from the trail head to the top, the view of the Blue Ridge mountains off in the distance is spectacular. Our logo encompasses the Cherokee word “Yonah”, meaning black bear.
+
+At YONAH Outdoors, we understand that outdoor gear can become very complex and with many parts. That’s why we are committed to simplifying the gear needed for any outdoor adventure. 
+Through experience and research, we have found ways to improve upon the current design for hammock camping in both function and safety. Tying knots in a piece of rope drastically weakens the rope fibers, leaving more potential for danger. For that reason, we have ditched the rope and designed our hammock system after slings and webbing used in rock climbing equipment, which is designed to withstand large amounts of force. These issues are what motivated us to design the YONAH hammock.
+
+Our hammock and suspension system will minimize the pieces needed and the complexity of remembering to bring everything on your adventure.
+
+We hope you enjoy our hammock as much as we do and remember, when you need to get out and breathe beneath the trees…</p>
+
+        <!-- Team Members Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="my-4">Our Team</h2>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center mb-4">
+                <img class="rounded-circle img-fluid d-block mx-auto" src="../img/Nathan-Colbert.png" alt="">
+                <h3>Nathan Colbert
+                    <small>Owner</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center mb-4">
+                <img class="rounded-circle img-fluid d-block mx-auto" src="../img/Ryan-Barger.png" alt="">
+                <h3>Ryan Barger
+                    <small>Owner</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <!-- <div class="col-lg-4 col-sm-6 text-center mb-4">
+                <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+                <h3>John Smith
+                    <small>Job Title</small>
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div> -->
+        </div>
+            
+    </div>
+    <!-- /.container -->
+
+    <?php include("../templates/footer.php"); ?>
+</body>
+
+</html>
---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<head>
+
+    <meta name="description" content="Hammocks, Shirts, Kids Hammocks, Compression Pillows">
+    <meta name="author" content="Yonah Outdoors">
+
+    <title>Yonah Outdoors Store</title>
+
+    <?php include("../templates/head.php"); ?>
+
+</head>
+
+<body>
+
+    <?php include("../templates/admin-navbar.php"); ?>
+
+    <div class="form">
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit
+        </button>
+      </form>
+
+    </div>
+
+
+</body>
+
+
+ ?>
